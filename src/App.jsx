ebd9b6c75f1dd29c74e7af53a39ff762cdc887dd
Nancy <nancyahmed2001@gmail.com>
2024-06-07 13:11:32 +0300

    adding bid item

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,48 @@ import Footer from "./components/Footer";
 import HomePage from "./components/HomePage";
 import AboutPage from "./components/AboutPage.jsx";
 import { Routes, Route } from "react-router-dom";
+import ItemBid from "./components/ItemBidPage";
+import AllItems from "./components/AllItemsPage";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        {/* <Route path="/item" element={<HomePage />} /> */}
-        {/* <Route path="/account" element={<HomePage />} /> */}
-        {/* <Route path="/signup" element={<HomePage />} /> */}
-        {/* <Route path="/login" element={<HomePage />} /> */}
-      </Routes>
-      <Footer />
-    </div>
-  );
+   const items = [
+      {
+         id:1,
+         image: "/item.jpg",
+         description: "Product 1",
+         startingPrice: 100,
+         reservedPrice: 500,
+         startDate: "1/1/24",
+         endDate: "7/7/25",
+      },
+      {
+         id:2,
+         image: "/item.jpg",
+         description: "Product 2",
+         startingPrice: 100,
+         reservedPrice: 500,
+         startDate: "1/1/24",
+         endDate: "7/7/25",
+      },
+   ];
+   return (
+      <div>
+         <Header />
+
+         {/* <ItemBid items={items} /> */}
+         <AllItems products={items}/>
+
+         <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            {/* <Route path="/item" element={<HomePage />} /> */}
+            {/* <Route path="/account" element={<HomePage />} /> */}
+            {/* <Route path="/signup" element={<HomePage />} /> */}
+            {/* <Route path="/login" element={<HomePage />} /> */}
+         </Routes>
+         <Footer />
+      </div>
+   );
 }
 
 export default App;
---
+++ src/components/ItemBidPage.jsx
@@ -0,0 +1,101 @@
+import "../App.css";
+import React, { useRef, useState } from "react";
+
+function ItemBid(props) {
+   // const Items = {
+   //    image: "/item.jpg",
+   //    description: "game controller",
+   //    details: "play station 4 game controller in good condition ",
+   //    startingPrice: 100,
+   //    reservedPrice: 500,
+   //    startDate: "1/1/24",
+   //    endDate: "7/7/24",
+   // };
+   const { Items } = props;
+   console.log("Items--->",Items);
+   const [currentBid, setCurrentBid] = useState(Items.startingPrice);
+   const [errorMessage, setErrorMessage] = useState(""); // State for error message
+
+   const [isBidOpen, setIsBidOpen] = useState(() => {
+      const now = new Date();
+      return now >= new Date(Items.startDate) && now <= new Date(Items.endDate);
+   }); // Check if bidding is open based on start date
+   // const isBiddingOpen = () => {
+   //    const now = new Date();
+   //    return now >= new Date(Items.startDate) && now <= new Date(Items.endDate);
+   // };
+   const handleBidChange = (e) => {
+      e.preventDefault();
+      setErrorMessage("");
+      console.log(e);
+      console.log("current bid", currentBid);
+      const newBid = parseFloat(e.target[0].value);
+      console.log("new bid", newBid);
+
+      if (newBid > currentBid && newBid > Items.startingPrice) {
+         setCurrentBid(newBid);
+         console.log("current bid: ", currentBid);
+      } else {
+         console.log("new bid", newBid);
+         setErrorMessage("Bid must be higher than current bid and starting price");
+
+         //  alert("Bid must be higher than current bid and starting price");
+      }
+   };
+   const buyNow = () => {
+      if (confirm("want to continue buying the item?")) {
+         setIsBidOpen(false);
+         console.log("Thing was saved to the database.");
+      } else {
+         // Do nothing!
+         console.log("Thing was not saved to the database.");
+      }
+   };
+
+   return (
+      // <main class="container">
+      // <div class="product-card">
+      // <img src={Items.image} alt={Items.description} />
+      //   <h3>Product Title 1</h3>
+      //   <p>A short description of product 1.</p>
+      //   <button>Add to Cart</button>
+      // </div>
+
+      // </main>
+      <div className="item">
+         <div className="item-img-container">
+            <img className="item-img" src={Items.image} alt={Items.description} />
+         </div>
+         <div className="item-details-container">
+            <div className="item-details">
+               <h3>{Items.description}</h3>
+               <p>Starting Price: {Items.startingPrice}LE</p>
+               <p>Reserved Price: {Items.reservedPrice}LE</p>
+               <p>Start Date: {new Date(Items.startDate).toLocaleDateString()}</p>
+               <p>End Date: {new Date(Items.endDate).toLocaleDateString()}</p>
+               <div>
+                  <p>{Items.details}</p>
+               </div>
+
+               {isBidOpen ? (
+                  <div className="bid-section">
+                     <p>Current Bid: {currentBid}LE</p>
+                     <form onSubmit={handleBidChange}>
+                        <input type="number" />
+                        <span className="error-message">{errorMessage}</span>
+                        <button disabled={currentBid >= Items.reservedPrice}>Place Bid</button>
+                     </form>
+                     <button id="buy-now-btn" onClick={buyNow}>
+                        Buy now at {Items.reservedPrice}LE
+                     </button>
+                  </div>
+               ) : (
+                  <p>Bidding Closed</p>
+               )}
+            </div>
+         </div>
+      </div>
+   );
+}
+
+export default ItemBid;
